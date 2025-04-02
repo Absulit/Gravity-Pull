a1c509ef4c601021e3240f6d059f057cf9896ef8
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-04-01 19:53:42 -0600

    threejs base

--- src/main.js
+++ src/main.js
@@ -12,14 +12,36 @@ playBtn.addEventListener('click', _ => {
 })
 
 
+
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+
+
+
 function update() {
 
     a.analyser?.getByteFrequencyData(a.data);
     // console.log(a.data);
 
 
-    requestAnimationFrame(update)
+    renderer.render(scene, camera);
+    // requestAnimationFrame(update)
 }
+renderer.setAnimationLoop(update);
 
 
 update();
