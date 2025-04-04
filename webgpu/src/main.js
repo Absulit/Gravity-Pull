a0eb6d80eb02421df714e198f810321c181af013
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-04-03 20:43:54 -0600

    working first visualizer

--- webgpu/src/main.js
+++ webgpu/src/main.js
@@ -12,8 +12,24 @@ import vert from './renderpasses/renderpass0/vert.js';
  */
 const points = new Points('canvas');
 
+
+
 points.setSampler('imageSampler', null);
 points.setTexture2d('feedbackTexture', true);
+
+
+let volume = 1;
+let loop = true;
+const audio = points.setAudio('audio', './../80s-pulse-synthwave-dude-212407.mp3', volume, loop, false);
+// const audio = points.setAudio('audio', './../synthwave-80s-robot-swarm-218092.mp3', volume, loop, false);
+// const audio = points.setAudio('audio', './../mezhdunami-fading-echoes-129291.mp3', volume, loop, false);
+
+// points.addEventListener('click_event', data => {
+//     audio.play();
+// }, 4);
+audio.play();
+points.setStorage('result', 'array<f32, 10>', 4);
+
 
 // create your render pass with three shaders as follow
 const renderPasses = [
@@ -22,6 +38,9 @@ const renderPasses = [
 
 // call the POINTS init method and then the update method
 await points.init(renderPasses);
+
+// points.fitWindow = true;
+
 update();
 
 // call `points.update()` methods to render a new frame
--- webgpu/src/renderpasses/renderpass0/frag.js
+++ webgpu/src/renderpasses/renderpass0/frag.js
@@ -1,26 +1,74 @@
 import { fnusin } from 'animation';
+import { texturePosition } from 'image';
+import { rotateVector } from 'math';
+import { snoise } from 'noise2d';
+import { sdfCircle, sdfLine2, sdfSegment } from 'sdf';
 
 const frag = /*wgsl*/`
 
 ${fnusin}
+${sdfSegment}
+${sdfLine2}
+${texturePosition}
+${snoise}
+${sdfCircle}
+${rotateVector}
 
 @fragment
 fn main(
-    @location(0) color: vec4<f32>,
-    @location(1) uv: vec2<f32>,
-    @location(2) ratio: vec2<f32>,  // relation between params.screen.x and params.screen.y
-    @location(3) uvr: vec2<f32>,    // uv with aspect ratio corrected
-    @location(4) mouse: vec2<f32>,
-    @builtin(position) position: vec4<f32>
-) -> @location(0) vec4<f32> {
+    @location(0) color: vec4f,
+    @location(1) uv: vec2f,
+    @location(2) ratio: vec2f,  // relation between params.screen.x and params.screen.y
+    @location(3) uvr: vec2f,    // uv with aspect ratio corrected
+    @location(4) mouse: vec2f,
+    @builtin(position) position: vec4f
+) -> @location(0) vec4f {
 
-    let cellSize = 20. + 10. * fnusin(1.);
-    let a = sin(uvr.x  * cellSize) * sin(uvr.y * cellSize);
-    let b = sin(uvr.x * uvr.y * 10. * 9.1 * .25 );
-    let c = fnusin(uvr.x * uvr.y * 10.);
-    let d = distance(a,b);
-    let f = d * uvr.x * uvr.y;
-    let finalColor:vec4<f32> = vec4(a*d,f*c*a,f, 1.);
+    // params.audioLength 1024
+    let audioX = audio.data[ u32(uvr.x * 800)] / 256;
+
+    let n = snoise(uvr * 2 + params.time * .01);
+    let s = sdfCircle(.5 * ratio, .1, .5, uvr);
+    let t = sdfCircle(.5 * ratio, .1, .00001, -uvr);
+
+    // let feedbackColor = texturePosition(feedbackTexture, imageSampler, vec2(), uvr  * vec2f(1, s), true);
+
+    let uvrRotate = rotateVector(uvr, params.time * .001 * audioX);
+    let feedbackColor = texturePosition(feedbackTexture, imageSampler, vec2(), uvrRotate  / vec2f(1, 1.01), true);
+
+
+
+
+    // let segmentNum = 4;
+    // let subSegmentLength = i32(params.audioLength) / segmentNum;
+
+    // for (var index = 0; index < segmentNum ; index++) {
+    //     var audioAverage = 0.;
+    //     for (var index2 = 0; index2 < subSegmentLength; index2++) {
+    //         let audioIndex = index2 * index;
+
+    //         let audioValue = audio.data[audioIndex] / 256;
+    //         audioAverage += audioValue;
+    //     }
+    //     result[index] = audioAverage / f32(subSegmentLength);
+    // }
+
+
+
+
+
+
+
+
+
+
+
+    let height = 0.;
+    // let uvrRotateLine = rotateVector(uvr, params.time * .01);
+    let l = sdfLine2(vec2(0, height + audioX) * ratio, vec2(1, height + audioX) * ratio, .005, uvr);
+
+    let finalColor = vec4f(l,l*audioX,0,1) + feedbackColor * .98;
+    // let finalColor = vec4f(1,s,0,1);
 
     return finalColor;
 }
