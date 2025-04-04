b9d7c8ca5c1e5707d27ef0f9803fca1a8331ff72
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-04-03 19:14:19 -0600

    render pass and other changes

--- webgpu/src/main.js
+++ webgpu/src/main.js
@@ -3,59 +3,21 @@
 
 import Points from 'points';
 import RenderPass from 'renderpass';
-import { fnusin } from 'animation';
+import frag0 from './renderpasses/renderpass0/frag.js';
+import vert from './renderpasses/renderpass0/vert.js';
 
 // reference the canvas in the constructor
-const points = new Points('gl-canvas');
+/**
+ * @type {Points}
+ */
+const points = new Points('canvas');
+
+points.setSampler('imageSampler', null);
+points.setTexture2d('feedbackTexture', true);
 
 // create your render pass with three shaders as follow
 const renderPasses = [
-    new RenderPass(/*wgsl*/`
-                @vertex
-                fn main(
-                    @location(0) position: vec4<f32>,
-                    @location(1) color: vec4<f32>,
-                    @location(2) uv: vec2<f32>,
-                    @builtin(vertex_index) vertexIndex: u32
-                ) -> Fragment {
-
-                    return defaultVertexBody(position, color, uv);
-                }`,
-                /*wgsl*/`
-                ${fnusin}
-                @fragment
-                fn main(
-                    @location(0) color: vec4<f32>,
-                    @location(1) uv: vec2<f32>,
-                    @location(2) ratio: vec2<f32>,  // relation between params.screen.x and params.screen.y
-                    @location(3) uvr: vec2<f32>,    // uv with aspect ratio corrected
-                    @location(4) mouse: vec2<f32>,
-                    @builtin(position) position: vec4<f32>
-                ) -> @location(0) vec4<f32> {
-
-                    let cellSize = 20. + 10. * fnusin(1.);
-                    let a = sin(uvr.x  * cellSize) * sin(uvr.y * cellSize);
-                    let b = sin(uvr.x * uvr.y * 10. * 9.1 * .25 );
-                    let c = fnusin(uvr.x * uvr.y * 10.);
-                    let d = distance(a,b);
-                    let f = d * uvr.x * uvr.y;
-                    let finalColor:vec4<f32> = vec4(a*d,f*c*a,f, 1.);
-
-                    return finalColor;
-                }
-                `,
-                /*wgsl*/`
-
-                @compute @workgroup_size(8,8,1)
-                fn main(
-                    @builtin(global_invocation_id) GlobalId: vec3<u32>,
-                    @builtin(workgroup_id) WorkGroupID: vec3<u32>,
-                    @builtin(local_invocation_id) LocalInvocationID: vec3<u32>
-                ) {
-                    let time = params.time;
-                }
-                `,
-    )
+    new RenderPass(vert, frag0, null),
 ];
 
 // call the POINTS init method and then the update method
