8544516a36804133535e35e7d52ec68f06b29092
Sebastian Sanabria Diaz <sebastian@absulit.com>
2025-04-05 11:28:13 -0600

    creating options

--- webgpu/src/renderpasses/renderpass0/frag.js
+++ webgpu/src/renderpasses/renderpass0/frag.js
@@ -28,41 +28,20 @@ fn main(
 
     // params.audioLength 1024
     let audioX = audio.data[ u32(uvr.x * 800)] / 256;
+    let audio0 = audio.data[ u32(.5 * 800)] / 256;
 
     let n = snoise(uvr * 2 + params.time * .01);
-    let s = sdfCircle(.5 * ratio, .1, .5, uvr);
+    let s = sdfCircle(.5 * ratio, .5 * audio0, .1, uvr);
     let t = sdfCircle(.5 * ratio, .1, .00001, -uvr);
 
     // let feedbackColor = texturePosition(feedbackTexture, imageSampler, vec2(), uvr  * vec2f(1, s), true);
 
     let center = vec2(.5) * ratio;
-    let uvrRotate = rotateVector(uvr - center, PI * .001) + center;
-    let feedbackColor = texturePosition(feedbackTexture, imageSampler, vec2(), uvrRotate / vec2f(1.01, 1.01), true);
+    let uvrRotate = rotateVector(uvr - center, PI * .001) + center; // option 1
+    // let uvrRotate = rotateVector(uvr - center, s) + center; // option 2
 
-
-
-
-    // let segmentNum = 4;
-    // let subSegmentLength = i32(params.audioLength) / segmentNum;
-
-    // for (var index = 0; index < segmentNum ; index++) {
-    //     var audioAverage = 0.;
-    //     for (var index2 = 0; index2 < subSegmentLength; index2++) {
-    //         let audioIndex = index2 * index;
-
-    //         let audioValue = audio.data[audioIndex] / 256;
-    //         audioAverage += audioValue;
-    //     }
-    //     result[index] = audioAverage / f32(subSegmentLength);
-    // }
-
-
-
-
-
-
-
-
+    // let feedbackColor = texturePosition(feedbackTexture, imageSampler, vec2(), uvrRotate / vec2f(1.01 * s, 1.01 / s) , true);
+    let feedbackColor = texturePosition(feedbackTexture, imageSampler, vec2(), uvrRotate / vec2f(1, 1.01), true);
 
 
 
@@ -70,7 +49,7 @@ fn main(
     // let uvrRotateLine = rotateVector(uvr, params.time * .01);
     let l = sdfLine2(vec2(0, height + audioX) * ratio, vec2(1, height + audioX) * ratio, .005, uvr);
 
-    let finalColor = vec4f(l,l*audioX,0,1) + feedbackColor * .98;
+    let finalColor = vec4f(l,l*audioX, l*uvrRotate.x, 1) + feedbackColor * .98;
     // let finalColor = vec4f(1,s,0,1);
 
     return finalColor;
